72fcfdcde366e506a1e4c0349335c3479660203a
Daniel Maynard <danielmaynard7@outlook.com>
2026-05-16 22:33:18 +0100

    Enum and matrix getters are public and H, S & T gates added

--- src/gates.rs
+++ src/gates.rs
@@ -1,16 +1,21 @@
+use std::f64::consts::SQRT_2;
+
 use ndarray::{array, Array2};
 use num_complex::Complex64;
 
 use crate::math_utils::C64;
 
-enum Gate64 {
+pub enum Gate64 {
     X,
     Y,
-    Z
+    Z,
+    H,
+    S,
+    T
 }
 
 impl Gate64 {
-    fn matrix(&self) -> Array2<Complex64> {
+    pub fn matrix(&self) -> Array2<Complex64> {
         match self {
             Gate64::X => array![
                 [C64(0., 0.), C64(1., 0.)],
@@ -23,7 +28,25 @@ impl Gate64 {
             Gate64::Z => array![
                 [C64(1., 0.), C64(0., 0.)],
                 [C64(0., 0.), C64(-1., 0.)]
-            ]
+            ],
+            Gate64::H => {
+                let c = C64(SQRT_2, 0.);
+                c*array![
+                    [C64(1., 0.), C64(1., 0.)],
+                    [C64(1., 0.), C64(-1., 0.)]
+                ]
+            },
+            Gate64::S => array![
+                [C64(1., 0.), C64(0., 0.)],
+                [C64(0., 0.), C64(0., 1.)]
+            ],
+            Gate64::T => {
+                let c = 1.0 / SQRT_2;
+                array![
+                    [C64(1., 0.), C64(0., 0.)],
+                    [C64(0., 0.), C64(c, c)]
+                ]
+            }
         }
     }
 }
