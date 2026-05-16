e8406ae2c6c2f8207e9dd7433c08bb86e858bb4a
Daniel Maynard <danielmaynard7@outlook.com>
2026-05-16 23:02:23 +0100

    Basic statevector with zero constructor and single-qubit gate application

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,8 @@
 use ndarray::Array1;
 use num_complex::Complex64;
 
+use crate::{gates::Gate64, math_utils::kron};
+
 pub struct State {
     amplitudes: Array1<Complex64>,
     circuit_size: usize
@@ -17,5 +19,31 @@ impl State {
             amplitudes,
             circuit_size
         }
+    }
+
+    pub fn amplitudes(&self) -> &[Complex64] {
+        return self.amplitudes.as_slice().expect("Array1 should always be contiguous")
+    }
+
+    pub fn apply_gate(&mut self, target: usize, gate: Gate64) -> Result<(), &str> {
+        if target > self.circuit_size - 1 {
+            return Err("Target qubit does not exist")
+        }
+
+        let identity = Gate64::I.matrix();
+
+        // Construct full circuit matrix and apply
+        let mut matrix = if target == 0 { gate.matrix() } else { identity.clone() };
+
+        for q in 1..self.circuit_size {
+            if q == target {
+                matrix = kron(&matrix, &gate.matrix())
+            } else {
+                matrix = kron(&matrix, &identity)
+            }
+        }
+        self.amplitudes = matrix.dot(&self.amplitudes);
+
+        Ok(())
     }
 }
